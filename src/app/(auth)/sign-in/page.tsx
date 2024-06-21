e3869364b75b027d1e12e1621cfa40d0617e7c28
Tanuj1718 <125687187+Tanuj1718@users.noreply.github.com>
2024-06-21 21:37:51 +0530

    arrange files and improve sign-in page

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,9 +1,108 @@
-import React from 'react'
+'use client'
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import * as z from "zod"
+import Link from "next/link"
+import { useEffect, useState } from "react"
+import { useDebounceCallback } from 'usehooks-ts'
+import { useToast } from "@/components/ui/use-toast"
+import { useRouter } from "next/navigation"
+import { signInSchema } from "@/schemas/signInSchema"
+import { signUpSchema } from "@/schemas/signUpSchema"
+import axios, {AxiosError} from 'axios'
+import { ApiResponse } from "@/types/ApiResponse"
+import { Loader2 } from "lucide-react"
+import { Button } from "@/components/ui/button"
+import { Form, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
+import { Input } from '@/components/ui/input';
+import { signIn } from "next-auth/react"
 
-function page() {
+const signin  = ()=> {
+  const [isSubmitting, setIsSubmitting] = useState(false)
+  const {toast} = useToast()
+  const router = useRouter()
+
+  //zod implementation
+  const form  = useForm<z.infer<typeof signInSchema>>({
+    resolver: zodResolver(signInSchema),
+    defaultValues: {
+      identifier: '',
+      password: '',
+    }
+  })
+
+
+  const onSubmit = async (data: z.infer<typeof signInSchema >)=>{
+    setIsSubmitting(true)
+    const result = await signIn('credentials',{
+      redirect: false,
+      identifier: data.identifier,
+      password: data.password
+    })
+    if(result?.error){
+      toast({
+        title: "Login failed",
+        description: "Incorrect username or password",
+        variant: "destructive"
+      })
+    }
+
+    if(result?.url){
+      router.replace('/dashboard')
+    }
+  }
+
   return (
-    <div>sign-in page</div>
+<div className="flex justify-center items-center min-h-screen bg-gray-800">
+      <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+        <div className="text-center">
+          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+            Join True Feedback
+          </h1>
+          <p className="mb-4">Sign in to start your anonymous adventure</p>
+        </div>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+            <FormField
+              name="identifier"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <Input {...field} name="email" placeholder="email/username" />
+                  <p className='text-muted text-gray-400 text-sm'>We will send you a verification code</p>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              name="password"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <Input type="password" {...field} name="password" placeholder="password"/>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit" className='w-full'>
+              Sign In
+            </Button>
+          </form>
+        </Form>
+        <div className="text-center mt-4">
+          <p>
+            Newbie to this app?{' '}
+            <Link href="/sign-up" className="text-blue-600 hover:text-blue-800">
+              Sign up
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
   )
 }
 
-export default page
+export default signin
